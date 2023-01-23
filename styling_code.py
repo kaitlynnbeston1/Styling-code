b536e0194a19c20aefa5708e3c9341fa210ad438
kaitlynnbeston1 <113719685+kaitlynnbeston1@users.noreply.github.com>
2023-01-23 09:29:17 -0600

    Added code to file. Somehow forgot to transfer. Oops.

---
+++ styling_code.py
@@ -0,0 +1,7 @@
+# List of pizzas with one added to show line length
+pizzas = ["hawaiian", "spinech and feta", "cheese", "vegetarian", "all dressed"]
+
+
+for pizza in pizzas:
+        print(f"I like {pizza} pizza.")
+        print("There is so much variety in pizza, it's hard to choose just one as a favorite.")
